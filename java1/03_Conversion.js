e0e14ca03170c61f0fe9dd1dee61e4572283b53b
kalapee <35555664+kalapee@users.noreply.github.com>
2023-09-30 17:27:58 +0000

    Conversion

---
+++ java1/03_Conversion.js
@@ -0,0 +1,12 @@
+let score = "88"
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "88" => 88
+// "88abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
