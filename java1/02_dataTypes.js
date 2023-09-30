205dde19987334966202e5fbac522b413d746b2c
kalapee <35555664+kalapee@users.noreply.github.com>
2023-09-30 16:53:29 +0000

    datatypes

---
+++ java1/02_dataTypes.js
@@ -0,0 +1,11 @@
+let name = "kalapee"
+let age = 43
+let isLoggedIn = false
+let state;
+
+
+console.log(typeof "kalapee")
+
+console.log(typeof age);
+
+console.log(typeof null);
---
+++ java1/index.js
@@ -0,0 +1,11 @@
+const accountId = 144880
+let accountEmail = "kalapee@google.com"
+var accountPassword = "123456"
+accountCity = "Aahmedabad"
+
+accountEmail = "gajjar@google.com"
+accountPassword = "8888000"
+accountCity = "Indoer"
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
